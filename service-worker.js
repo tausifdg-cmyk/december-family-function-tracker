ab1518e16f9ca5c56ee0a72afa8f06875166cbc4
tausifdg-cmyk <tausifdg@gmail.com>
2026-08-10 18:15:02 +0200

    fix: refresh PWA cache for multi-food nutrition update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='december-tracker-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./plan.js','./manifest.json','./data/plan.json','./data/exercises.json','./icons/icon-192.svg','./icons/icon-512.svg'];
+const CACHE='december-tracker-v3';
+const ASSETS=['./','./index.html','./style.css','./multi-food.css','./app.js','./plan.js','./food-data.js','./multi-food.js','./manifest.json','./data/plan.json','./data/exercises.json','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
