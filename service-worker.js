be91caca3eea94de54be8f0cba167488d4a69459
tausifdg-cmyk <tausifdg@gmail.com>
2026-08-13 00:00:54 +0200

    Bump PWA cache for same-screen HealthKit bridge

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='december-tracker-v6';
+const CACHE='december-tracker-v7';
 const ASSETS=['./','./index.html','./style.css','./app.js','./food-data.js','./health-sync.js','./manifest.json','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
