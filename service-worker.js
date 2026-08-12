3798c67155e1e360cdce533d3777870b8909a5db
tausifdg-cmyk <tausifdg@gmail.com>
2026-08-12 23:48:47 +0200

    Cache Apple Health sync bridge

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='december-tracker-v5';
-const ASSETS=['./','./index.html','./style.css','./app.js','./food-data.js','./manifest.json','./icons/icon-192.svg','./icons/icon-512.svg'];
+const CACHE='december-tracker-v6';
+const ASSETS=['./','./index.html','./style.css','./app.js','./food-data.js','./health-sync.js','./manifest.json','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(x=>x||caches.match('./index.html'))))});
