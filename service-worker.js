ff2c4b31689727cfd63765611a86a9692ef86e62
tausifdg-cmyk <tausifdg@gmail.com>
2026-08-13 00:25:39 +0200

    Refresh PWA cache after removing exercise visuals

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='december-tracker-v10';
+const CACHE='december-tracker-v11';
 const ASSETS=['./','./index.html','./style.css','./app.js','./food-data.js','./health-sync.js','./workout-videos.js','./ui-upgrades.js','./entry-recovery.js','./manifest.json','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
